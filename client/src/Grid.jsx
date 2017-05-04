d29d257922a218636dbbd416804b2d7c85d1e4b8
Alice Loudon <alice_loudon@icloud.com>
2017-05-04 18:43:52 +0100

    Display 9 squares

--- client/src/Grid.jsx
+++ client/src/Grid.jsx
@@ -3,10 +3,36 @@ import Square from './Square'
 
 class Grid extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: [ 
+        {index: 0},
+        {index: 1},
+        {index: 2},
+        {index: 3},
+        {index: 4},
+        {index: 5},
+        {index: 6},
+        {index: 7},
+        {index: 8},
+        ],
+      currentPlayer: 'x'
+    }
+  }
+
   render(){
+
+    const gridSquares = this.state.squares.map( (square, index) => {
+      return <Square key={index}></Square>
+    })
+
     return (
       <div>
         <h1>Tic Tac Toe</h1>
+        <div id='grid'>
+          {gridSquares}
+        </div>
       </div>
     )
   }
